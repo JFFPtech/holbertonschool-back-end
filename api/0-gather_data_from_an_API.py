1f2e7d45c4b7adcb727737bc3f987df0de475f79
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-04-15 07:09:46 -0700

    added if to check user

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,5 +8,11 @@ def get_todo_list_data(employee_id):
     """ Get the todo list data from a given employee id """
     
     base_url = 'https://jsonplaceholder.typicode.com'
-    user_respone = requests.get(f"{base_url}/users/{employee_id}")
-    user_data = user_respone.json()
+
+    user_response = requests.get(f"{base_url}/users/{employee_id}")
+    user_data = user_response.json()
+
+    if not user_data:
+        print(f"No employee record found for ID: {employee_id}")
+        return
+    
