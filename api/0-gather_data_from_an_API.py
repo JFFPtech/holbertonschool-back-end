7f0d5fbccb41b01b6867d863dbdde06de4a032a7
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-04-15 14:39:43 -0700

    reformtatted to only include main

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,37 +4,22 @@
 import requests
 import sys
 
-def get_todo_list_data(employee_id):
-    """ Get the todo list data from a given employee id """
-    
-    base_url = 'https://jsonplaceholder.typicode.com'
-
-    user_response = requests.get(f"{base_url}/users/{employee_id}")
-    user_data = user_response.json()
-
-    if not user_data:
-        print(f"No employee record found for ID: {employee_id}")
-        return
-    
-    todo_response = requests.get(f"{base_url}/todos", params={'userId': employee_id})
-    todo_data = todo_response.json()
-
-    total_tasks = len(todo_data)
-    completed_tasks = sum(1 for task in todo_data if task['completed'])
-
-    employee_name = user_data.get('name')
-
-    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
-    for task in todo_data:
-        if task['completed']:
-            print(f"\t {task['title']}")
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-    try:
-        employee_id = int(sys.argv[1])
-        get_todo_list_data(employee_id)
-    except ValueError:
-        print("Employee ID must be an integer!")
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(sys.argv[1])).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                        .format(sys.argv[1])).json()
+    
+    tasks_done = 0
+    total_tasks = 0
+    completed_tasks = []
+    for task in todo:
+        if task['completed']:
+            tasks_done += 1
+            completed_tasks.append(task['title'])
+        total_tasks += 1
+    
+    print("Employee {} is done with tasks({}/{}):"
+            .format(user.get('name'), tasks_done, total_tasks))
+    for task in completed_tasks:
+        print("\t {}".format(task))
