20e14bda51e77d6bed235c91fc820fc9e23ca121
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-04-15 14:46:36 -0700

    test 2

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-""" Gather data from an API """
-
+"""Gather data from an API."""
 import requests
 from sys import argv
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
-    response = requests.get(url)
-    user = response.json()
 
-    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1])
-    response = requests.get(url)
+if len(argv) == 2:
+    employee_id = argv[1]
 
-    todos = response.json()
-    completed = [todo for todo in todos if todo.get('completed') is True]
-    print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
-                                                            len(completed),
-                                                            len(todos)))
-    [print("\t {}".format(todo.get('title'))) for todo in completed]
+    full_url = "https://jsonplaceholder.typicode.com/"
+    employee_info = full_url + "users/{}".format(employee_id)
+    todo_url = full_url + "todos?userId={}".format(employee_id)
+
+    employee_get = requests.get(employee_info)
+    todo_get = requests.get(todo_url)
+
+    employee_data = employee_get.json()
+    todo_data = todo_get.json()
+
+    employee_name = employee_data["name"]
+    num_total_tasks = len(todo_data)
+    num_done_tasks = sum(task["completed"] for task in todo_data)
+    completed_tasks = [task["title"]
+                       for task in todo_data if task["completed"]]
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, num_done_tasks, num_total_tasks))
+    for task_title in completed_tasks:
+        print("\t {}".format(task_title))
