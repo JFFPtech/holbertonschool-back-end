89a42f3666599330656511a16194189d4aa35c60
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-04-15 15:18:28 -0700

    changes to og verison2

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,35 +4,34 @@
 import requests
 import sys
 
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-    
-    employee_id = sys.argv[1]
+employee_id = sys.argv[1]
 
-    user_response = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{employee_id}')
-    user_data = user_response.json()
+user_response = requests.get(
+    'https://jsonplaceholder.typicode.com/users/' + employee_id)
 
-    if not user_data:
-        print(f'No employee record found for ID: {employee_id}')
-        sys.exit(1)
+user_data = user_response.json()
 
-    employee_name = user_data['name']
+if not user_data:
+    print('No employee record found for ID: {}'.format(employee_id))
+    sys.exit()
 
-    todos_response = requests.get(
-        f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
-    todos_data = todos_response.json()
+employee_name = user_data['name']
 
-    total_todos = len(todos_data)
-    completed_todos = sum(task['completed'] for task in todos_data)
+todos_response = requests.get(
+    'https://jsonplaceholder.typicode.com/todos?userId=' + employee_id)
 
-    print(f'Employee {employee_name} is done with tasks({completed_todos}/{total_todos}):')
+todos_data = todos_response.json()
 
-    for task in todos_data:
-        if task['completed']:
-            print(f'\t {task["title"]}')
+total_todos = str(len(todos_data))
+
+completed_todos = str(sum(1 for task in todos_data if task['completed']))
+
+print('Employee' + employee_name + ' is done with tasks(' + 
+        completed_todos + '/' + total_todos + '):')
+
+for task in todos_data:
+    if task['completed']:
+        print('\t ' + task['title'])
 
 if __name__ == '__main__':
-    main()
+    pass
