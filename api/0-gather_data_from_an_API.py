3b437e8ad6163233b05e4d4b62333ee7235804c3
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-04-16 20:48:36 -0700

    pycode

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,44 +1,40 @@
 #!/usr/bin/python3
-"""Module to gather data from an API using requests to fetch JSON data."""
+""" Gather data from an API """
 
 import requests
 import sys
 
-
 def get_todo_list_data(employee_id):
-    """Retrieve and display todo list data for a given employee ID."""
+    """ Get the todo list data from a given employee id """
+    
     base_url = 'https://jsonplaceholder.typicode.com'
 
-    user_url = f"{base_url}/users/{employee_id}"
-    user_response = requests.get(user_url)
+    user_response = requests.get(f"{base_url}/users/{employee_id}")
     user_data = user_response.json()
 
     if not user_data:
         print(f"No employee record found for ID: {employee_id}")
         return
-
-    todo_params = {'userId': employee_id}
-    todo_url = f"{base_url}/todos"
-    todo_response = requests.get(todo_url, params=todo_params)
+    
+    todo_response = requests.get(f"{base_url}/todos", params={'userId': employee_id})
     todo_data = todo_response.json()
 
     total_tasks = len(todo_data)
     completed_tasks = sum(1 for task in todo_data if task['completed'])
+
     employee_name = user_data.get('name')
 
-    print(f"Employee {employee_name} completed "
-          f"{completed_tasks}/{total_tasks} tasks:")
+    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
     for task in todo_data:
         if task['completed']:
             print(f"\t {task['title']}")
 
-
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: script.py <employee_id>")
+        print("Usage: 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
     try:
         employee_id = int(sys.argv[1])
         get_todo_list_data(employee_id)
     except ValueError:
-        print("Employee ID must be an integer.")
+        print("Employee ID must be an integer")
